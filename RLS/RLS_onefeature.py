f368ae8f5c461f94880cfc45734fce4d56a99722
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2021-11-17 20:24:15 +0530

    v2

--- RLS/RLS_onefeature.py
+++ RLS/RLS_onefeature.py
@@ -7,7 +7,7 @@ y = 4 + 3*X +np.random.randn(100,1)
 # concatenate for w0
 Xwb = np.c_[np.ones((100,1)), X]
 
-# Compute w directly
+# Computing w directly
 W = Xwb.T.dot(Xwb)
 W = np.linalg.inv(W)
 W = (W.dot(Xwb.T)).dot(y)
